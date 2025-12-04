da8b274be623992098d3aa9f75db5f81f1a1df3c
Odunayo <odunayoogunleye200@gmail.com>
2025-12-04 07:11:25 +0100

    feat: enhance dashboard with new components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -27,10 +27,10 @@ backdrop-blur-xl shadow-xl overflow-hidden">
   {/* Top Accent Line */}
   <div className="h-1 bg-linear-to-r from-slate-800 via-slate-600 to-cyan-500" />
 
-  <div className="p-6 sm:p-8 flex flex-row-reverse max-sm:flex-col gap-6">
+  <div className="p-6 sm:p-8 flex max-sm:flex-col-reverse flex-row-reverse  gap-6">
 
     {/* LEFT PANEL */}
-    <div className=" w-[60%] lg:col-span-2 space-y-4">
+    <div className=" sm:w-[60%] lg:col-span-2 space-y-4">
 
       {/* Status */}
       <span className="inline-flex items-center gap-2
@@ -41,7 +41,7 @@ backdrop-blur-xl shadow-xl overflow-hidden">
 
       {/* Title */}
       <h2 className="text-2xl sm:text-3xl font-bold text-slate-900 dark:text-white">
-        Welcome back, {user.firstName}
+        Welcome back, {user.lastName}
       </h2>
 
       {/* Info */}
@@ -60,7 +60,7 @@ backdrop-blur-xl shadow-xl overflow-hidden">
     </div>
 
     {/* RIGHT PANEL */}
-    <div className="w-[40%] bg-slate-50 dark:bg-slate-800/60
+    <div className="w-[80%] sm:w-[40%] bg-slate-50 dark:bg-slate-800/60
     border border-slate-200 dark:border-slate-700 rounded-2xl p-5
     flex flex-col items-center justify-center text-center space-y-4">
 
