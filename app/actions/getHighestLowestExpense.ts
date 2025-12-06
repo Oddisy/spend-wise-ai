65eca4d6a98e6ef8bee0e5ddfec7a03b236c0a7d
Odunayo <odunayoogunleye200@gmail.com>
2025-12-06 13:39:53 +0100

    feat: add getHighestLowestExpense action and integrate into dashboard

---
+++ app/actions/getHighestLowestExpense.ts
@@ -0,0 +1,42 @@
+'use server';
+import { checkUser } from '@/lib/checkuser';
+import { db } from '@/lib/db';
+
+async function getHighestLowestExpense(): Promise<{
+  highestExpense?: number;
+  lowestExpense?: number;
+  error?: string;
+}> {
+  const  user  = await checkUser();
+  const userId = user?.clerkUserid;
+
+  if (!user) {
+    return { error: 'User not found' };
+  }
+
+  try {
+    // Fetch all records for the authenticated user
+    const records = await db.record.findMany({
+      where: { userId},
+      select: { amount: true },
+    });
+    console.log('Fetched records for expense calculation:', records);
+
+    if (!records || records.length === 0) {
+      return { highestExpense: 0, lowestExpense: 0 }; // Return 0 if no records exist
+    }
+
+    const amounts = records.map((record) => record.amount);
+
+    // Calculate best and worst expense amounts
+    const highestExpense = Math.max(...amounts); // Highest amount
+    const lowestExpense = Math.min(...amounts); // Lowest amount
+
+    return { highestExpense, lowestExpense };
+  } catch (error) {
+    console.error('Error fetching expense amounts:', error); // Log the error
+    return { error: 'Database error' };
+  }
+}
+
+export default getHighestLowestExpense;
