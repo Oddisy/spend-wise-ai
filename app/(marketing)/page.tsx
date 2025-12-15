7ec2336710fa12057d974e2ca54a3187d05ccd0e
Odunayo <odunayoogunleye200@gmail.com>
2025-12-15 08:32:40 +0100

    feat: improved folder structure for marketing and dashboard

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,17 @@
+import { checkUser } from "@/lib/checkuser";
+import GuestPage from "./guest/page";
+import Dashboard from "../(dashboard)/dashboard/page";
+
+export default async function  HomePage(){
+  const user = await checkUser()
+  if(!user){
+    return <GuestPage/>
+
+  }
+  return (
+    <div>
+      <Dashboard />
+    </div>
+
+  )
+}
